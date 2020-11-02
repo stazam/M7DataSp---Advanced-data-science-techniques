48c9ad3c4f201288b5cca7a7073d9f2adc9ec631
stazam <zamecnik.stanislav@gmail.com>
2020-11-02 20:29:34 +0100

    Add raw link

--- docs/index.js
+++ docs/index.js
@@ -12,7 +12,7 @@ async function app() {
   console.log('Loading mobilenet..');
 
   // Load the model.
-  net = await tf.loadLayersModel('https://github.com/stazam/M7DataSp---Advanced-data-science-techniques/blob/main/docs/export_model/model.json');
+  net = await tf.loadLayersModel('https://raw.githubusercontent.com/stazam/M7DataSp---Advanced-data-science-techniques/main/docs/export_model/model.json');
   console.log('Successfully loaded model');
 
   // Make a prediction through the model on our image.
